96d8ec266dd09dc9895416991322eeb989a8f7b5
ikurakin <ivancurachin@gmail.com>
2018-09-28 23:05:12 +0200

    fix translate api url env name

--- config/config.go
+++ config/config.go
@@ -7,7 +7,7 @@ type Config struct {
 	ServiceName     string `env:"SERVICE_NAME" envDefault:"chupapintos_translate_bot"`
 	TranslateApiKey string `env:"TRANSLATE_API_KEY" envDefault:""`
 	TelegramBotKey  string `env:"TELEGRAM_BOT_KEY" envDefault:""`
-	TranslateApiURL string `env:"TELEGRAM_BOT_KEY" envDefault:"https://translate.yandex.net/api/v1.5/tr.json/translate"`
+	TranslateApiURL string `env:"TRANSLATE_API_URL" envDefault:"https://translate.yandex.net/api/v1.5/tr.json/translate"`
 	LangugeSrc      string `env:"LANGUAGE_SRC" envDefault:"pt"`
 	LanguageDst     string `env:"LANGUAGE_DST" envDefault:"en"`
 }
